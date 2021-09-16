5b79d8700076834cd57b23c81cd78a0bd5e43bbf
nyccowgirl <nyc_cowgirl@hotmail.com>
2021-09-16 03:02:57 -0500

    added working draft of Comparators. modifications of builder pattern.

---
+++ src/Transactions.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+
+public abstract interface Transactions {
+
+    public BigDecimal calculate(BigDecimal balance, BigDecimal interest);
+
+}
+
+class Interest implements Transactions {
+
+    @Override
+    public BigDecimal calculate(BigDecimal balance, BigDecimal interest) {
+        return balance.multiply(interest.add(BigDecimal.valueOf(1)));
+    }
+}
+
+class Fees implements Transactions {
+
+    @Override
+    public BigDecimal calculate(BigDecimal balance, BigDecimal fees) {
+        return balance.subtract(fees);
+    }
+}
+
+class Bonus implements Transactions {
+
+    @Override
+    public BigDecimal calculate(BigDecimal balance, BigDecimal bonus) {
+        return balance.add(bonus);
+    }
+}
